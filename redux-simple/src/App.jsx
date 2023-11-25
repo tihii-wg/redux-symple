d0a14aba0aa2bc8e0e933b1210319e6e25fe1732
Vova <podolian@gmail.com>
2023-11-25 16:23:22 +0200

    new

---
+++ redux-simple/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+
+
+	return (
+		<div className='wraper'>
+			<h1 className='logo'>
+				Redux.
+			</h1>
+			<div className='cash'>
+				0
+			</div>
+			<div className='container'>
+				<button>Add.</button>
+				<button>Get.</button>
+				<button>Add users.</button>
+				<button>Remove users.</button>
+			</div>
+			<h2 className='empty_logo'>No users.</h2>
+		</div>
+	)
+}
+
+export default App
